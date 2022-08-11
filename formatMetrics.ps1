1d2422a15d0c04262953f741a28e40cf116ccfdc
SebSebD <sebastien.dillier@squadra-run.com>
2022-08-11 16:41:02 +0200

    add ps1 stage

---
+++ formatMetrics.ps1
@@ -0,0 +1,10 @@
+param(
+    [Parameter(Mandatory=$true)]
+    #[ValidatePattern('^[a-zA-Z0-9]+$')]
+    [String] $typeOfAlertstoFormat
+   
+)
+
+Write-Host $typeOfAlertstoFormat
+#$ResourceGroupName = "$($Prefix)rg"
+
