ec061f70f24f90800ac2c3f43c1c05cffa633c21
SebSebD <sebastien.dillier@squadra-run.com>
2022-08-12 10:19:15 +0200

    editpws

--- formatMetrics.ps1
+++ formatMetrics.ps1
@@ -7,4 +7,27 @@ param(
 
 Write-Host $typeOfAlertstoFormat
 #$ResourceGroupName = "$($Prefix)rg"
+if($typeOfAlertstoFormat -ne "Activity"){
+    $csvFileToConvert=import-csv -Delimiter ";" "AlertsDefinitions/$typeOfAlertstoFormatAlerts.csv"
+    foreach($alertLine in $csvFileToConvert){
+        if($alertLine.alertDimensions -ne ""){
+            $dimensionsArray=@()
+            $dimensions = $alertLine.alertDimensions.Split(',')
+        
+            foreach($dimension in $dimensions){
+                $splittedDimension = $dimension.Split('|')
+                $dimensionsArray+=@{name = $splittedDimension[0];  operator = $splittedDimension[1]; values = ($splittedDimension[2] -ne "") ? @($splittedDimension[2].split('!')):"*" } 
+            }
+       
+            $alertLine.alertDimensions = $dimensionsArray
+            $alertLineIndex = $csvFileToConvert.IndexOf($alertLine)
+            $csvFileToConvert[$alertLineIndex].alertDimensions= $alertLine.alertDimensions 
+        }
+    }
 
+    $formattedAlerts = $csvMetrics | ConvertTo-Json -AsArray -Depth 4
+    ((Get-Content -path "AlertsDefinitions/$typeOfAlertstoFormatAlerts.json" -Raw) -replace '"--TOREPLACE--"',$formattedAlerts ) | Set-Content -Path "AlertsDefinitions/$typeOfAlertstoFormatAlerts.json"
+    Get-Content -path "AlertsDefinitions/$typeOfAlertstoFormatAlerts.json" -Raw
+}else{
+    write-host "activitylogs deployment"
+}
