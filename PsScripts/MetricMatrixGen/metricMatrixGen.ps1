f1826958ab06f1dce6decb7f713ed0c85b1006c4
SebSebD <sebastien.dillier@squadra-run.com>
2022-09-02 10:27:06 +0200

    matrixgenstruct

---
+++ PsScripts/MetricMatrixGen/metricMatrixGen.ps1
@@ -0,0 +1,85 @@
+<#
+    ---SUPMATRIXGEN v1.0---
+    1 > Edit exclude.json to exclude the metrics you don't need #https://docs.microsoft.com/en-us/azure/azure-monitor/essentials/metrics-supported
+    2 > Connect-AzAccount to the subscription
+    3 > .\creatematrice.ps1 -csvOutputPath "yourpath.csv" -verboseOutput
+    
+#>
+###IMPLEMENT ADMIN ALERTS (ACTIVITYLOG) + KQL QUERIES + CREER SCRIPT AUDIT DIAG SETTINGS AND CHECK MULTI REGION
+param(
+    [Parameter()]
+    #verbose Output ==> True
+    #filtered on resourceTypesOnly => False
+    [bool]$verboseOutput = $true,
+ 
+    [Parameter()]
+     # path of the output csv file
+    [string]$csvOutputPath = 'matrix.csv'
+)
+
+    
+
+
+$RGs = Get-AZResource
+
+
+$suggestedPercentage = 10
+$exclude = Get-Content -Path .\exclude.json | ConvertFrom-JSON
+$outputObject = @()
+
+foreach ($rg in $RGs) {
+
+
+    $resourcesTypesInRG = Get-AzResource -ResourceGroupName $rg | Select-Object -Unique -Property ResourceType -WarningAction SilentlyContinue
+
+    
+    foreach($resourceTypeInRG in $resourcesTypesInRG){
+    $resourcesByTypeInRg = Get-AzResource -ResourceGroupName $rg -ResourceType $resourceTypeInRG.ResourceType -WarningAction SilentlyContinue
+    ####NO METRICS FOR FOLLOWING RESOURCES TYPES store in json later or do it by api call ?####
+    if(!$resourceTypeInRG.ResourceType.Contains('networkSecurityGroups') -and !$resourceTypeInRG.ResourceType.Contains('virtualMachines/extensions') -and !$resourceTypeInRG.ResourceType.Contains('extensions') -and !$resourceTypeInRG.ResourceType.Contains('registries/replications')  -and !$resourceTypeInRG.ResourceType.Contains('virtualNetworkLinks')){ #skip Microsoft.Compute/virtualMachines/extensions
+        foreach ($resource in $resourcesByTypeInRg) {
+            try {
+                write-host $resourceTypeInRG.ResourceType
+                $metrics =  Get-AzMetricDefinition -ResourceId $resource.ResourceId -WarningAction SilentlyContinue  
+                 foreach($metric in $metrics)
+                {
+
+                    ##TRESHOLD SUGGESTION##
+                    $metricValOnLastWeek = (Get-AzMetric -ResourceId $resource.ResourceId -TimeGrain 1.00:00:00 -StartTime (Get-Date).AddDays(-7) -AggregationType Average -MetricName $metric.Name.Value).Data | Select-Object Average
+                    $metricHighestValOnLastWeek = ($metricValOnLastWeek.Average | Measure-Object   -Maximum).Maximum
+                    $suggestedTresholdVal = $metricHighestValOnLastWeek + ($metricHighestValOnLastWeek*$suggestedPercentage) / 100 
+
+                    $metricSplitted = $metric.ResourceId -split('/')
+                    $propToCheck = $metricSplitted[6]+"/"+$metricSplitted[7].replace(' ','')
+                    $res= ($exclude.PSObject.Properties.Match($propToCheck).count -eq 1 -and $exclude.$propToCheck.Contains($metric.Name.Value) -eq $true )
+                    if(!$res){
+                        if($verboseOutput){
+                            $outputObject += [PSCustomObject]@{targetResourceName=$resource.Name;resourceRG=$rg;targetResourceType=$resourceTypeInRG.ResourceType;alertDescription="";alertMetricNameSpace=$propToCheck;alertMetricName=$metric.Name.Value;alertSev=1;alertDimensions="";alertOperator="GreaterThanOrEqual";alertTimeAggregation="Average";evaluationFreq="PT5M";windowsSize="PT30M";alertTreshold=$suggestedTresholdVal} ###retreivemtricunit to make it dynamic
+                        }else{
+                            $outputObject += [PSCustomObject]@{MtricNamespace=$propToCheck;MetricValue=$metric.Name.Value;Sev=1;EvaluationFreq="PT5M";TimeWindow="PT30M";TresHold="XX";} ###retreivemtricunit to make it dynamic
+                        }
+                    }
+                } 
+           
+             
+            }
+            catch {
+                Write-Host "Execution finished with an error..." -ForegroundColor Red
+                Write-Host $errors[0]
+            }
+        } 
+
+    }
+
+    }
+   
+
+}
+
+#$outputObject
+ if($verboseOutput){
+    $outputObject | Export-Csv -NoTypeInformation $csvOutputPath
+}else{
+    $outputObject | Sort-Object -Property MtricNamespace,MetricValue -Unique | Export-Csv -NoTypeInformation $csvOutputPath
+}
+ 
