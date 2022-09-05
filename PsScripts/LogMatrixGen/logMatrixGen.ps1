7a4b6346ca0535de99c18d7be383d027f4c89ce9
Romain Rodrigues <romain.rodrigues@squadra-run.com>
2022-09-05 14:33:10 +0000

    Added logMatrixGen.ps1

---
+++ PsScripts/LogMatrixGen/logMatrixGen.ps1
@@ -0,0 +1,45 @@
+#Créer une matrice CSV où l'on récupère les ressources de la subscription, puis on y ajoute les KQL logs associés (voir DicoLogs.json)
+
+## On converti notre JSON en Array dans une variable $Logs
+$Logs = Get-Content -Raw DicoLogs.json | ConvertFrom-Json
+
+$outputObject = @()
+
+## On récupère tous les resources de notre subscription
+$Resources = Get-AzResource
+
+## Boucle sur les resources de la subscription
+foreach($Resource in $Resources){
+    $ResourceType = Get-AzResource -ResourceId $Resource.Id
+    
+    ## Boucle sur élements du JSON
+    foreach($Log in $Logs){
+        
+        ## Si Le nom de la TypeResource et égal au nom de la clé Resource du JSON, alors :
+        if($ResourceType.ResourceType -eq $Log.resource){
+            $index = $Logs.resource.IndexOf($Log.resource)
+            Write-Host $Log.resource
+            
+            ## Boucle sur la taille de la query de la resource en question
+            for ($i=0; $i -lt $Logs[$index].queries.Length; $i++){
+                $outputObject += [PSCustomObject]@{
+                    targetResourceName = $Resource.Name
+                    ResourceRG = $Resource.ResourceGroupName
+                    targetResourceType = $ResourceType.ResourceType
+                    targetResourceTypeFriendlyName = ""
+                    alertDescription = ""
+                    alertSev = 1
+                    alertOperator = "GreaterThanOrEqual"
+                    alertTimeAggregation = "Total"
+                    evaluationFreq = "PT1M"
+                    windowsSize = "PT5M"
+                    alertTreshold = ""
+                    alertQuery = $Logs[$index].queries[$i] -join ","
+                }
+            }
+        }
+    }
+}
+
+## Export en CSV
+$outputObject | Export-Csv -Path ".\MatriceLogsKQL.csv" -Delimiter ";" -NoTypeInformation
